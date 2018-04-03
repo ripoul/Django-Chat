75523bfbc4a4e99f02230324b123ca3b849dc7d8
LE BRIS Jules <jls.lebris@gmail.com>
2018-04-03 22:24:02 +0200

    create user working

--- chat/views.py
+++ chat/views.py
@@ -9,6 +9,12 @@ from django.contrib.auth import logout
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth.models import User
+from .models import Profile
+
+import string
+import random
+
 
 @login_required(login_url='/chat/login/')
 def index(request):
@@ -40,7 +46,33 @@ def disconect(request):
 
 
 def createUser(request):
-    
-    user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+    username = request.POST['username']
+    firstname = request.POST['firstname']
+    lastname = request.POST['lastname']
+    email = request.POST['email']
+    password = request.POST['password']
+
+    ppstr = handle_uploaded_file(request.FILES['pp'])
+
+    user = User.objects.create_user(username=username,
+                                 email=email,
+                                 password=password)
+    user.first_name=firstname
+    user.last_name=lastname
+    user.profile.photo=ppstr
+    user.save()
 
     return HttpResponse("wip")
+
+
+def handle_uploaded_file(f):
+    title = 'chat/static/pp/'+id_generator()+'.jpeg'
+    with open(title, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+        
+    return title
+
+
+def id_generator(size=6, chars=string.ascii_uppercase):
+    return ''.join(random.choice(chars) for _ in range(size))
